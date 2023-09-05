1731b159b643f331ae329ee023d0e732e957e2b0
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-09-05 02:59:13 -0700

    Update CRGToken.sol

--- contracts/CRGToken.sol
+++ contracts/CRGToken.sol
@@ -50,16 +50,10 @@ contract CRGToken is Context, IBEP20, Ownable {
         return _decimals;
     }
 
-    /**
-     * @dev Returns the token symbol.
-     */
     function symbol() external view returns (string memory) {
         return _symbol;
     }
 
-    /**
-    * @dev Returns the token name.
-    */
     function name() external view returns (string memory) {
         return _name;
     }
