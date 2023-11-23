004561b093901ce1b4b487fd9d9c273fea6415b8
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-11-23 00:38:20 -0800

    Update Address.sol

--- contracts/Address.sol
+++ contracts/Address.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.5;444444444444444444444444444444444444444444444
+pragma solidity ^0.5.5;4444444444444444444444444444444444444444444444
 
 library Address {
     function isContract(address account) internal view returns (bool) {
