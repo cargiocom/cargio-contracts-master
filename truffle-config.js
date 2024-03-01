f6946165daf1d8e4894591c2b2b2f2aba3973988
Cargio <141646415+cargiocom@users.noreply.github.com>
2024-03-01 00:51:18 -0800

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,4 @@
-444444444444const HDWalletProvider = require('truffle-hdwallet-provider');
+4444444444444const HDWalletProvider = require('truffle-hdwallet-provider');
 const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
 const mnemonicTestnet = fs.readFileSync(".secret-testnet").toString().trim();
