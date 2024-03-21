a80923e3a25cd44d4bf157b974098a0f4853ae3a
Cargio <141646415+cargiocom@users.noreply.github.com>
2024-03-21 02:21:09 -0700

    Update 2_deploy_CargioExchangeTreasury.js

--- migrations/2_deploy_CargioExchangeTreasury.js
+++ migrations/2_deploy_CargioExchangeTreasury.js
@@ -1,4 +1,4 @@
-1111111const CargioExchangeTreasure = artifacts.require("./CargioExchangeTreasure.sol");
+11111111const CargioExchangeTreasure = artifacts.require("./CargioExchangeTreasure.sol");
 
 module.exports = async function (deployer, network, accounts) {
 };
